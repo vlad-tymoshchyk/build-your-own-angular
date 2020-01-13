f245abc1e84910eed4949f8e6aa4765bb2adb5ee
Vlad Tymoshchyk <vtymoshchyk@gmail.com>
2020-01-14 00:31:18 +0200

    added first test for scope

--- src/scope.js
+++ src/scope.js
@@ -1,5 +1,21 @@
-module.exports = {
-  module(name) {
-    console.log('registered new: ', name);
-  }
+function Scope() {
+  this.$$watchers = [];
 }
+
+function initWatchVal() {};
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  this.$$watchers.push({
+    watchFn,
+    listenerFn,
+    last: initWatchVal,
+  });
+};
+Scope.prototype.$digest = function() {
+  this.$$watchers.forEach(watcher => {
+    const newVal = watcher.watchFn(this);
+    const oldVal = watcher.last;
+    watcher.listenerFn(newVal, oldVal, this);
+  })
+}
+module.exports = Scope;
--- test/scope.test.js
+++ test/scope.test.js
@@ -1,8 +1,50 @@
-const angular = require('../src/scope');
+const Scope = require('../src/scope');
 
-describe('test', () => {
-  test('angular.module is a function', () => {
-    expect(typeof angular.module).toBe('function');
+describe('Scope', () => {
+  test('it can be constructed and used as an object', () => {
+    const scope = new Scope();
+    scope.aProperty = 1;
+    expect(scope.aProperty).toBe(1);
+  });
+
+  describe('digest', () => {
+    let scope;
+
+    beforeEach(() => {
+      scope = new Scope();
+    });
+
+    test('calls the listener function on first $digest', () => {
+      const watchFn = jest.fn();
+      const listenerFn = jest.fn();
+      scope.$watch(watchFn, listenerFn);
+      scope.$digest();
+      expect(listenerFn).toHaveBeenCalled();
+      expect(watchFn).toHaveBeenCalledWith(scope);
+    });
+
+    test('calls the listener function when the scope value changes', () => {
+      scope.someVal = 'a';
+      scope.counter = 0;
+
+      scope.$watch(
+        scope => scope.someVal,
+        (newVal, oldVal, scope) => { scope.counter++ },
+      );
+
+      expect(scope.counter).toBe(0);
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      scope.someVal = 'b';
+      expect(scope.counter).toBe(1);
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+      // scope.$digest();
+      // expect(scope.counter).toBe(2);
+    })
+    test('calls listener with new valu as old value for first time', () => {
+
+
+    })
   });
 });
-
